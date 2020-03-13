fdef82553d2999f6c5d5f456dd7f5ec39818d975
WilliamSokolErhard <WilliamSokolErhard@users.noreply.github.com>
2020-03-13 15:19:13 -0700

    turn & throw

--- Assets/AIBotController.cs
+++ Assets/AIBotController.cs
@@ -14,12 +14,15 @@ public class AIBotController : MonoBehaviour
     public AgentState agentState = AgentState.Patrol; 
 
     CharacterController characterController;
+    Animator animator;
 
     public float speed = 6.0f;
     public float jumpSpeed = 8.0f;
     public float gravity = 9.8f;
 
-    public Transform waypoints;
+    public List<Transform> enemies = new List<Transform>();
+    //private Transform enemyTarget;
+    public Transform waypointsParent;
     private int waypointTarget = 0;
     private Transform waypointTargetTransform;
 
@@ -30,12 +33,29 @@ public class AIBotController : MonoBehaviour
 
     private Vector3 moveDirection = Vector3.zero;
 
+    void OnDrawGizmos()
+    {
+        // Draw a yellow sphere at the transform's position
+        Gizmos.color = new Color(0,1,0,0.2f);
+        foreach(Transform waypoint in waypointsParent)
+        {
+            Gizmos.DrawSphere(waypoint.position, 0.25f);
+        }        
+    }
+
     void Start()
     {
         characterController = GetComponent<CharacterController>();
-        waypointTargetTransform = waypoints.GetChild(waypointTarget);
+        animator = GetComponent<Animator>();
+        waypointTargetTransform = waypointsParent.GetChild(waypointTarget);
     }
 
+    private Transform ClosestEnemy()
+    {
+        return enemies[0]; //TODO
+    }
+
+    float timer = -1.8f;
     void Update()
     {
         Vector3 target = Vector3.zero;
@@ -48,52 +68,61 @@ public class AIBotController : MonoBehaviour
                 {
                     if (loop)
                     {
-                        waypointTarget = waypointTarget + 1 >= waypoints.childCount ? 0 : waypointTarget + 1;
+                        waypointTarget = waypointTarget + 1 >= waypointsParent.childCount ? 0 : waypointTarget + 1;
                     }
                     else
                     {
-                        if ((increasing && waypointTarget + 1 >= waypoints.childCount) || (!increasing && waypointTarget - 1 <= 0))
+                        if ((increasing && waypointTarget + 1 >= waypointsParent.childCount) || (!increasing && waypointTarget - 1 <= 0))
                         {
                             increasing = !increasing;
-                            transform.rotation *= Quaternion.Euler(0, 35, 0);
+                            animator.SetTrigger("AboutFace");
+                            timer = Time.time;
+                            print("AboutFace");
+                            //transform.rotation *= Quaternion.Euler(0, 35, 0);
                         }
 
                         waypointTarget += increasing ? 1 : -1;
                     }
-                    waypointTargetTransform = waypoints.GetChild(waypointTarget);
+                    waypointTargetTransform = waypointsParent.GetChild(waypointTarget);
                 }
 
                 target = new Vector3(waypointTargetTransform.position.x, transform.position.y, waypointTargetTransform.position.z) - transform.position;
                 break;
             case AgentState.Attack:
-
+                animator.SetTrigger("EngageEnemy");
+                if (Vector3.Distance(transform.position, ClosestEnemy().position)<10f) { 
+                    target = new Vector3(ClosestEnemy().position.x, transform.position.y, ClosestEnemy().position.z) - transform.position;
+                    animator.SetTrigger("AttackEnemy"); 
+                }
                 break;
             case AgentState.Cover:
 
                 break;            
         }
 
-
-        var step = rotationSpeed * Time.deltaTime;
-        RaycastHit RH;
-        Vector3 rayHeight = transform.position + Vector3.up / 1.5f;
-        if (Physics.Raycast(rayHeight, target, out RH, 1.5f) || Physics.Raycast(transform.position + Vector3.up / 1.5f, transform.forward, 1.5f))
+        if (timer + 2f <= Time.time)
         {
-            step *= 4f;
-            if (!Physics.Raycast(rayHeight, transform.forward - transform.right, out RH, 3f))
+            var step = rotationSpeed * Time.deltaTime;
+            RaycastHit RH;
+            Vector3 rayHeight = transform.position + Vector3.up / 1.5f;
+            if (Physics.Raycast(rayHeight, target, out RH, 1.5f) || Physics.Raycast(transform.position + Vector3.up / 1.5f, transform.forward, 1.5f))
             {
-                target = Quaternion.Euler(0, -45, 0) * target;
+                step *= 4f;
+                if (!Physics.Raycast(rayHeight, transform.forward - transform.right, out RH, 3f))
+                {
+                    target = Quaternion.Euler(0, -45, 0) * target;
+                }
+                else if (!Physics.Raycast(rayHeight, transform.forward + transform.right, out RH, 3f))
+                {
+                    target = Quaternion.Euler(0, 45, 0) * target;
+                }
             }
-            else if (!Physics.Raycast(rayHeight, transform.forward + transform.right, out RH, 3f))
-            {
-                target = Quaternion.Euler(0, 45, 0) * target;
-            }
-        }
-        Debug.DrawRay(rayHeight, target);
+            Debug.DrawRay(rayHeight, target);
 
-        // Rotate our transform a step closer to the target's.
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(target, Vector3.up), step);
-        //transform.LookAt(new Vector3(waypointTargetTransform.position.x,transform.position.y,waypointTargetTransform.position.z));
+            // Rotate our transform a step closer to the target's.
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(target, Vector3.up), step);
+            //transform.LookAt(new Vector3(waypointTargetTransform.position.x,transform.position.y,waypointTargetTransform.position.z));
+        }
 
         if (characterController.isGrounded)
         {
